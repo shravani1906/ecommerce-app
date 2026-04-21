e4cc47507b87325766310b5a15ef1c246a6cc5f6
shravani196 <shravanijamsandekar06@gmail.com>
2026-04-21 14:06:27 +0530

    fixed frontend url

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,7 @@ const server = http.createServer(app);
 // SOCKET
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://wick-weave-frontend.onrender.com",
-    ],
+    origin: ["http://localhost:5173", "https://wick-and-weave.netlify.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
@@ -28,10 +25,7 @@ app.set("io", io);
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://wick-weave-frontend.onrender.com",
-    ],
+    origin: ["http://localhost:5173", "https://wick-and-weave.netlify.app/"],
     credentials: true,
   }),
 );
@@ -69,6 +63,4 @@ io.on("connection", (socket) => {
 
 // SERVER START
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () =>
-  console.log(`🔥 Server running on http://localhost:${PORT}`),
-);
+server.listen(PORT, () => console.log(`🔥 Server running`));
